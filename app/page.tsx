7a95023f58bc4940ec1991112fb3e95ffe0b1f21
jacob moya <jemoya1994@Gmail.com>
2025-11-03 21:25:52 -0800

    tinkering with some 3D UI aspects - an outline of sorts - to indicate the 'hover' effect

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,78 @@
 "use client";
-import * as THREE from "three";
 import { OrbitControls, Stars } from "@react-three/drei";
 import { Canvas, useLoader } from "@react-three/fiber";
+import { useState } from "react";
+import * as THREE from "three";
+
+interface EarthProps {
+  color: string;
+}
+interface MoonProps {
+  color: string;
+}
 export default function Home() {
+  const [active, setActive] = useState<boolean>(false);
+  const [activeMoon, setActiveMoon] = useState<boolean>(false);
+
   const earthMap = useLoader(THREE.TextureLoader, "/earthMap.png");
   const moonMap = useLoader(THREE.TextureLoader, "moonMap.jpg");
+
   return (
     <main id="canvas-container" className="flex w-screen h-screen">
       <Canvas style={{ background: "black" }}>
         <group>
-          <mesh position={[0, 0, 0]}>
-            <sphereGeometry args={[1.4, 32]} />
-            <meshStandardMaterial map={earthMap} color="white" />
+          {active && (
+            <mesh scale={1.005}>
+              <sphereGeometry args={[1.4, 64, 64]} />
+              <meshBasicMaterial color="white" side={THREE.BackSide} />
+            </mesh>
+          )}
+
+          {activeMoon && (
+            <mesh scale={1.1} position={[5, 4, 1]}>
+              <sphereGeometry args={[0.2, 32]} />
+              <meshBasicMaterial color="white" side={THREE.BackSide} />
+            </mesh>
+          )}
+
+          <mesh
+            onPointerEnter={() => {
+              setActive(true);
+            }}
+            onPointerLeave={() => {
+              setActive(false);
+            }}
+            position={[0, 0, 0]}
+          >
+            <sphereGeometry args={[1.4, 64, 64]} />
+            <meshStandardMaterial map={earthMap} color={0xffffff} />
           </mesh>
 
-          <mesh position={[5, 4, 1]}>
+          <mesh
+            onPointerEnter={() => {
+              setActiveMoon(true);
+            }}
+            onPointerLeave={() => {
+              setActiveMoon(false);
+            }}
+            position={[5, 4, 1]}
+          >
             <sphereGeometry args={[0.2, 32]} />
             <meshStandardMaterial map={moonMap} color="white" />
           </mesh>
         </group>
+
         <Stars
           radius={100}
-          depth={50}
+          depth={10}
           count={5000}
-          factor={4}
+          factor={1}
           saturation={0}
           fade
           speed={1}
         />
 
-        <ambientLight intensity={1} />
-        <directionalLight position={[1, 1, 0]} color="white" intensity={1} />
-        <pointLight position={[1, 1, 0]} color="white" />
+        <directionalLight position={[1, 1, 1]} color="white" intensity={1.5} />
         <OrbitControls
           enableDamping={true}
           dampingFactor={0.1}
